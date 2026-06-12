5fb1e7229c4b30ebc3eef33cfe285196d30aa49e
Magan Jadhav <146972328+MaganJadhav@users.noreply.github.com>
2026-06-12 11:46:53 +0530

    Update TextGeneration.py

--- TextGeneration.py
+++ TextGeneration.py
@@ -1,1 +1,12 @@
+from transformers import pipeline
 
+p = pipeline("text-generation",model="gpt2")
+
+a=input("Enter text : ")
+
+r=p(a,max_length=100)
+
+print("\n")
+
+for i in r:
+  print(i['generated_text'])
